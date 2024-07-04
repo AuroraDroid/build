33534d99ae04c67e86f106296ff88e42a2eb1a6f
kamleshhv <kamleshvansajalia@gmail.com>
2024-07-04 07:25:39 +0000

    build: Add Auora build support
     Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Thu Aug 17 22:22:13 2017 +0200

--- envsetup.sh
+++ envsetup.sh
@@ -204,6 +204,13 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+if (echo -n $1 | grep -q -e "^aurora_") ; then
+        AURORA_BUILD=$(echo -n $1 | sed -e 's/^aurora_//g')
+    else
+        AURORA_BUILD=
+    fi
+    export AURORA_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_RELEASE= \
         TARGET_BUILD_VARIANT= \
@@ -817,6 +824,8 @@ function lunch()
         echo "Valid combos must be of the form <product>-<release>-<variant>"
         return 1
     fi
+
+ check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
